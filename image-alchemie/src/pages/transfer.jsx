eb3ff0014aa128275ac4ba8857c7699c6075a350
Franziska Rubenbauer <86341509+Sammy159@users.noreply.github.com>
2024-02-17 11:16:43 +0100

    New Order Structure

---
+++ image-alchemie/src/pages/transfer.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import DotsMobileStepper from "../components/transferstepper.jsx";
+
+export default function StyleTransfer() {
+  return (
+    <>
+      <DotsMobileStepper></DotsMobileStepper>
+    </>
+  );
+}

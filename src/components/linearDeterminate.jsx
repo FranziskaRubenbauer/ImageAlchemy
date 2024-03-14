86132bab6862f3397efc87959c0ac76efa13882a
Franziska Rubenbauer <86341509+Sammy159@users.noreply.github.com>
2024-03-14 17:01:32 +0100

    Ladebalken gefixt

--- src/components/linearDeterminate.jsx
+++ src/components/linearDeterminate.jsx
@@ -6,18 +6,27 @@ export default function LinearDeterminate() {
   const [progress, setProgress] = React.useState(0);
 
   React.useEffect(() => {
+    const duration = 140; // Zeit in Sekunden
+    const interval = 1000; // Intervall in Millisekunden
+    const steps = (duration * 1000) / interval; // Anzahl der Schritte
+    const diff = 100 / steps; // Schrittgröße
+
     const timer = setInterval(() => {
       setProgress((oldProgress) => {
-        const diff = 0.55;
         return Math.min(oldProgress + diff, 100);
       });
-    }, 1000);
+    }, interval);
 
-    if (progress === 100) {
-      return () => {
-        clearInterval(timer);
-      };
-    }
+    // Nach Ablauf der Zeit den Timer stoppen
+    const timeout = setTimeout(() => {
+      clearInterval(timer);
+    }, duration * 1000);
+
+    // Aufräumen
+    return () => {
+      clearInterval(timer);
+      clearTimeout(timeout);
+    };
   }, []);
 
   return (
